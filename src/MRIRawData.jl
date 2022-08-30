c78ccbb73ca19c0e8a7ce99188ad358cb87a8c7d
Felix Horger <felix.horger@kcl.ac.uk>
2022-08-30 20:33:29 +0100

    Outsourced coordinate stuff, improved code and comments.

--- src/MRIRawData.jl
+++ src/MRIRawData.jl
@@ -13,22 +13,27 @@ module MRIRawData
 	#	shape::NTuple{4, Int64}
 	#end
 
-	function twixread(path::AbstractString)
+	function readtwix(path::AbstractString; quiet=true)
 		!isfile(path) && error("File '$path' not found")
-		twix = siemens.mapVBVD(path; quiet=true)
+		twix = siemens.mapVBVD(path; quiet)
 		twix_image = twix["image"]
 		twix_image.squeeze = true
 		twix_image.flagRemoveOS = true
-		kspace = twix_image.unsorted(quiet=true)
-		num_columns = convert(Int, twix_image["NCol"]) ÷ 2 # F me
-		other_dims = convert.(
-			Int,
-			twix_image[key] for key in ("NLin", "NPar", "NCha")
-		)
-		sampling = [CartesianIndex(Int.((l, p))) for (l, p) in zip(twix_image.Lin, twix_image.Par)]
-		return twix, kspace, sampling, num_columns, other_dims...
+		kspace = twix_image.unsorted(;quiet)
+		num_columns		= convert(Int, twix_image["NCol"]) ÷ 2 # F me
+		num_lines		= convert.(Int, twix_image["NLin"])
+		num_partitions	= convert.(Int, twix_image["NPar"])
+		num_channels	= convert.(Int, twix_image["NCha"])
+		sampling = [CartesianIndex(Int.((l+1, p+1))) for (l, p) in zip(twix_image.Lin, twix_image.Par)]
+		return twix, kspace, sampling, num_columns, num_lines, num_partitions, num_channels
 	end
 	
+	"""
+		Returns dc, β, δx, Δx
+		Δx is translation of the centre of volume.
+		To get the origin, do origin = Δx .- R * (0.5 * δx .* [num_columns, num_lines, num_partitions])
+		where R is the rotation matrix of the volume obtained using MRICoordinates.ras (RAS coordinates)
+	"""
 	function twix_coordinates(twix)
 		twix_image = twix["image"]
 		bogus = twix["hdr"]["MeasYaps"]
@@ -46,8 +51,6 @@ module MRIRawData
 			key = (keys..., "sNormal", orientations[i])
 			dc[i] = get(bogus, key, 0.0)
 		end
-		φ = atan(dc[2], dc[1])
-		θ = atan(sqrt(dc[1]^2 + dc[2]^2), dc[3])
 		# In-plane rotation
 		β = let
 			key = (keys..., "dInPlaneRot")
@@ -59,7 +62,7 @@ module MRIRawData
 			key = (keys..., "sPosition", orientations[i])
 			Δx[i] = get(bogus, key, 0.0)
 		end
-		return φ, θ, β, δx, Δx
+		return dc, β, δx, Δx
 	end
 
 end

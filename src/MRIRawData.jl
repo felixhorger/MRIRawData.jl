734e25e96d6ba9a097f31cce352e8c002608b782
Felix Horger <felix.horger@kcl.ac.uk>
2022-10-05 17:40:22 +0100

    Solved bugs regarding size and FOV. Improved modularisation

--- src/MRIRawData.jl
+++ src/MRIRawData.jl
@@ -18,39 +18,93 @@ module MRIRawData
 		twix = siemens.mapVBVD(path; quiet)
 		return twix
 	end
-	function twix_kspace(twix; rm_oversampling=false, quiet=true)
+	function twix_kspace(twix; quiet::Bool=true, key::AbstractString="image")
 		# k-space
-		twix_image = twix["image"]
-		twix_image.squeeze = true
-		twix_image.flagRemoveOS = rm_oversampling
-		kspace = twix_image.unsorted(;quiet)
+		twix_obj = twix[key]
+		kspace = twix_obj.unsorted(;quiet)
+		return kspace
+	end
+	function twix_remove_oversampling(twix, b::Bool=true; key::AbstractString="image")
+		twix[key].flagRemoveOS = b
+		return
+	end
+	function twix_size(twix; key::AbstractString="image")
+		twix_obj = twix[key]
 		# Columns need extra care because of oversampling
-		num_columns	= convert(Int, twix_image["NCol"])
-		if rm_oversampling
+		num_columns	= convert(Int, twix_obj["NCol"])
+		if twix_obj.flagRemoveOS
 			num_columns ÷= 2
 		end
 		# Read other dimensions
-		num_lines		= convert.(Int, twix_image["NLin"])
-		num_partitions	= convert.(Int, twix_image["NPar"])
-		num_channels	= convert.(Int, twix_image["NCha"])
-		num_repetitions	= convert.(Int, twix_image["NRep"])
-		# Sampling indices
-		sampling = [CartesianIndex(Int.((l+1, p+1))) for (l, p) in zip(twix_image.Lin, twix_image.Par)]
-		return kspace, sampling, num_columns, num_lines, num_partitions, num_channels, num_repetitions
+		#=
+			Would be too easy if one could use
+
+			num_lines		= convert(Int, twix_obj["NLin"])
+			num_partitions	= convert(Int, twix_obj["NPar"])
+			num_channels	= convert(Int, twix_obj["NCha"])
+
+			but it happens that this doesn't match with the FOV that was selected, i.e.
+			this gives you the sampled k-space size, not necessarily matching the reconstructed one
+			I will refrain from using the language I want to use.
+		=#
+		bogus = twix["hdr"]["Meas"]
+		num_lines		= convert(Int, bogus["lPhaseEncodingLines"])
+		num_partitions	= convert(Int, bogus["lPartitions"])
+		num_channels	= convert(Int, twix_obj["NCha"])
+		return num_columns, num_lines, num_partitions, num_channels
 	end
+	function twix_sampling(twix; key::AbstractString="image")
+		twix_obj = twix[key]
+		return [CartesianIndex(Int.((l, p)) .+ 1) for (l, p) in zip(twix_obj.Lin, twix_obj.Par)]
+	end
+	function twix_index(twix, name::Symbol; key::AbstractString="image")
+		twix_obj = twix[key]
+		num_index = convert.(Int, twix_obj["N" * String(name)])
+		indices = 1 .+ Int.(getproperty(twix_obj, name))
+		return indices, num_index
+	end
+
+	for (param, twix_name) in (("TE", "alTE"), ("TR", "alTR"), ("FA", "adFlipAngleDegree"))
+		symb = Symbol(param)
+		func_name = Symbol("twix_" * param)
+		eval(Expr(:function,
+			Expr(:call, func_name, :twix, :(i::Integer)),
+			:(twix["hdr"]["MeasYaps"][$twix_name, string(i-1)])
+		))
+		eval(Expr(:function, Expr(:call, func_name, :twix), quote
+			bogus = twix["hdr"]["MeasYaps"]
+			$symb = Vector{Float64}(undef, 0)
+			i = 0
+			while true
+				key = ($twix_name, string(i))
+				v = get(bogus, key, 0) / 1000.0
+				v == 0 && break
+				push!($symb, v)
+				i += 1
+			end
+			return $symb
+		end))
+	end
+
+	twix_afi_TR_ratio(twix) = twix["hdr"]["MeasYaps"][("sWipMemBlock", "alFree", "10")]
+
 	
 	"""
 		Returns dc, β, fov, Δx
+		fov[1] (readout) is set according to twix["image"].flagRemoveOS
 		Δx is translation of the centre of volume.
 		To get the origin, do origin = Δx .- R * (0.5 .* fov)
 		where R is the rotation matrix of the volume obtained using MRICoordinates.ras (RAS coordinates)
+		β is inplane rotation
 	"""
-	function twix_coordinates(twix)
-		twix_image = twix["image"]
+	function twix_coordinates(twix; key::AbstractString="image")
 		bogus = twix["hdr"]["MeasYaps"]
 		keys = ("sSliceArray", "asSlice", "0") # Lord have mercy
 		# Scaling
 		fov = [ bogus[(keys..., dim)] for dim in ("dReadoutFOV", "dPhaseFOV", "dThickness") ]
+		if !twix[key].flagRemoveOS
+			fov[1] *= 2
+		end
 		# Rotation
 		dc = Vector{Float64}(undef, 3); # Direction cosines
 		orientations = ("dSag", "dCor", "dTra")

1a17a0c3b09f41b4a3bdddb2c659c720d401f120
Felix Horger <felix.horger@kcl.ac.uk>
2022-11-27 22:24:33 +0000

    SiemensRawData type for multiple dispatch. Added patient position (with MRICoordinates).

--- src/MRIRawData.jl
+++ src/MRIRawData.jl
@@ -1,36 +1,39 @@
 
 module MRIRawData
 
-	using PyCall
+	import PyCall
+	import Base: size, show
+	import MRICoordinates: PatientPosition
 
 	function __init__()
-		global siemens = pyimport("mapvbvd")
+		global siemens = PyCall.pyimport("mapvbvd")
 	end
 
-	# Music of the future, praying that the ISMRMRD format is good
-	#struct MRIRawDataSet{T} where T <: Number
-	#	kspace::AbstractArray{T, 3} # Readout, channel, phase encoding
-	#	shape::NTuple{4, Int64}
-	#end
-
-	# TODO: Naming of functions is not really julian
-	# Probably best to wrap twix into struct to give it a proper julia type to do multiple dispatch on
-	function readtwix(path::AbstractString; quiet=true)
-		!isfile(path) && error("File '$path' not found")
-		twix = siemens.mapVBVD(path; quiet)
-		return twix
+	"""
+		Wrapper for Siemens raw data.
+		This name is more informative than "twix".
+		Some of the internal code still uses the name twix to separate it from other formats.
+	"""
+	struct SiemensRawData
+		data::PyCall.PyObject
 	end
-	function twix_kspace(twix; quiet::Bool=true, key::AbstractString="image")
+
+	show(io::IO, _::SiemensRawData) = print("Siemens MRI raw data")
+	show(io::IO, ::MIME"text/plain", raw::SiemensRawData) = show(io, raw)
+	load_siemens(path::AbstractString; quiet=true) = SiemensRawData(siemens.mapVBVD(path; quiet))
+
+	function get_kspace(raw::SiemensRawData; quiet::Bool=true, key::AbstractString="image")
 		# k-space
-		twix_obj = twix[key]
+		twix_obj = raw.data[key]
 		kspace = twix_obj.unsorted(;quiet)
 		return kspace
 	end
-	function twix_remove_oversampling!(twix, b::Bool=true; key::AbstractString="image")
-		twix[key].flagRemoveOS = b
+	function remove_oversampling!(raw::SiemensRawData, b::Bool=true; key::AbstractString="image")
+		raw.data[key].flagRemoveOS = b
 		return
 	end
-	function twix_size(twix; key::AbstractString="image")
+	function size(raw::SiemensRawData; key::AbstractString="image")
+		twix = raw.data
 		twix_obj = twix[key]
 		# Columns need extra care because of oversampling
 		num_columns	= convert(Int, twix_obj["NCol"])
@@ -54,12 +57,12 @@ module MRIRawData
 		num_channels	= convert(Int, twix_obj["NCha"])
 		return num_columns, num_lines, num_partitions, num_channels
 	end
-	function twix_sampling(twix; key::AbstractString="image")
-		twix_obj = twix[key]
+	function get_sampling(raw::SiemensRawData; key::AbstractString="image")
+		twix_obj = raw.data[key]
 		return [CartesianIndex(Int.((l, p)) .+ 1) for (l, p) in zip(twix_obj.Lin, twix_obj.Par)]
 	end
-	function twix_index(twix, name::Symbol; key::AbstractString="image")
-		twix_obj = twix[key]
+	function get_sampling_index(raw::SiemensRawData, name::Symbol; key::AbstractString="image")
+		twix_obj = raw.data[key]
 		num_index = convert.(Int, twix_obj["N" * String(name)])
 		indices = 1 .+ Int.(getproperty(twix_obj, name))
 		return indices, num_index
@@ -67,13 +70,13 @@ module MRIRawData
 
 	for (param, twix_name, unit) in (("TE", "alTE", 0.001), ("TR", "alTR", 0.001), ("FA", "adFlipAngleDegree", π / 180.0))
 		symb = Symbol(param)
-		func_name = Symbol("twix_" * param)
+		func_name = Symbol("get_" * param)
 		eval(Expr(:function,
-			Expr(:call, func_name, :twix, :(i::Integer)),
-			:(twix["hdr"]["MeasYaps"][$twix_name, string(i-1)] * $unit)
+			Expr(:call, func_name, :(raw::SiemensRawData), :(i::Integer)),
+			:(raw.data["hdr"]["MeasYaps"][$twix_name, string(i-1)] * $unit)
 		))
-		eval(Expr(:function, Expr(:call, func_name, :twix), quote
-			bogus = twix["hdr"]["MeasYaps"]
+		eval(Expr(:function, Expr(:call, func_name, :(raw::SiemensRawData)), quote
+			bogus = raw.data["hdr"]["MeasYaps"]
 			$symb = Vector{Float64}(undef, 0)
 			i = 0
 			while true
@@ -87,19 +90,18 @@ module MRIRawData
 		end))
 	end
 
-	twix_afi_TR_ratio(twix) = twix["hdr"]["MeasYaps"][("sWipMemBlock", "alFree", "10")]
-
 	
 	"""
-		Returns dc, β, fov, Δx
-		dc ≡ direction cosines
+		Returns normal, β, fov, Δx
+		normal ≡ partition direction in patient coordinate system (LPS coordinates)
 		fov[1] (readout) is set according to twix["image"].flagRemoveOS
 		Δx is translation of the centre of volume.
 		To get the origin, do origin = Δx .- R * (0.5 .* fov)
-		where R is the rotation matrix of the volume obtained using MRICoordinates.ras (RAS coordinates)
-		β is inplane rotation
+		where R is the rotation matrix of the volume obtained using MRICoordinates.gradient2device
+		β is inplane rotation, clockwise
 	"""
-	function twix_coordinates(twix; key::AbstractString="image")
+	function get_coordinates(raw::SiemensRawData; key::AbstractString="image")
+		twix = raw.data
 		bogus = twix["hdr"]["MeasYaps"]
 		keys = ("sSliceArray", "asSlice", "0") # Lord have mercy
 		# Scaling
@@ -108,11 +110,11 @@ module MRIRawData
 			fov[1] *= 2
 		end
 		# Rotation
-		dc = Vector{Float64}(undef, 3); # Direction cosines
+		normal = Vector{Float64}(undef, 3); # Direction cosines
 		orientations = ("dSag", "dCor", "dTra")
 		for i = 1:3
 			key = (keys..., "sNormal", orientations[i])
-			dc[i] = get(bogus, key, 0.0)
+			normal[i] = get(bogus, key, 0.0)
 		end
 		# In-plane rotation
 		β = let
@@ -125,17 +127,43 @@ module MRIRawData
 			key = (keys..., "sPosition", orientations[i])
 			Δx[i] = get(bogus, key, 0.0)
 		end
-		return dc, β, fov, Δx
+		return normal, β, fov, Δx
 	end
 
-	const twix_num_WIPparameters = 14
+	"""
+		Patient position(ing)
+	"""
+	function get_patient_position(raw::SiemensRawData)
+		pos = raw.data["hdr"]["Config"]["PatientPosition"]
+		if pos == "HFS"
+			return HeadFirstSupine
+		elseif pos == "HFP"
+			return HeadFirstProne
+		elseif pos == "HFLR"
+			return HeadFirstLateralRight
+		elseif pos == "HFLL"
+			return HeadFirstLateralLeft
+		elseif pos == "FFS"
+			return FeetFirstSupine
+		elseif pos == "FFP"
+			return FeetFirstProne
+		elseif pos == "FFLR"
+			return FeetFirstLateralRight
+		elseif pos == "FFLL"
+			return FeetFirstLateralLeft
+		end
+		return InvalidPosition
+	end
+
 	"""
 		types: 0 for integer, 1 for double
+		The so called "WIP" parameters
 	"""
-	function twix_WIPparameters(twix)
-		bogus = twix["hdr"]["MeasYaps"]
-		params = Vector{Union{Int64, Float64}}(undef, twix_num_WIPparameters)
-		for i = 1:twix_num_WIPparameters
+	function get_special_parameters(raw::SiemensRawData)
+		num_WIPparameters = 14
+		bogus = raw.data["hdr"]["MeasYaps"]
+		params = Vector{Union{Int64, Float64}}(undef, num_WIPparameters)
+		for i = 1:num_WIPparameters
 			j = i - 1
 			key = ("sWipMemBlock", "alFree", "$j")
 			if haskey(bogus, key)
@@ -146,6 +174,8 @@ module MRIRawData
 		end
 		return params
 	end
+	# Special case, convenient
+	get_afi_TR_ratio(raw::SiemensRawData) = raw.data["hdr"]["MeasYaps"][("sWipMemBlock", "alFree", "10")]
 
 end
 
